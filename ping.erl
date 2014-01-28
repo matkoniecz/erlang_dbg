a7cf9e4cbc352a1f93c0109ed2619762a5302d6c
Bulwersator <bulwersator@gmail.com>
2014-01-28 13:58:38 +0100

    initial commit

---
+++ ping.erl
@@ -0,0 +1,36 @@
+-module(ping).
+-export([start/0, stop/0, play/1, ping/0, pong/0]).
+
+ping() ->
+	receive
+		0 ->
+			ping();
+		Number ->
+			io:format("ping ~w~n", [Number]),
+			pong ! Number-1,
+			ping()
+		after
+			20000 -> ok
+	end.
+
+pong() ->
+	receive
+		0 ->
+			pong();
+		Number ->
+			io:format("pong ~w~n", [Number]),
+			ping ! Number-1,
+			pong()
+		after
+			20000 -> ok
+	end.
+
+play(Number) ->
+	ping ! Number.
+	
+stop() -> ok.
+
+start() ->
+	register(ping, spawn(ping, ping, [])),
+	register(pong, spawn(ping, pong, [])).
+
